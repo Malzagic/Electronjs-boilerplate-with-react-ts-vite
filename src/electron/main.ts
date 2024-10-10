0aa3f6a6122e8af7efbe36c27821f18c53e9c317
Malzagic <przemek2142@gmail.com>
2024-10-10 19:52:50 +0200

    add feature: development envioroment, run all server and frontend of the app

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,12 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./util.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
 });
